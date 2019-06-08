0a6cf8b03efea703ea2dda092912528fc8012b24
sasabrina <sabrinaealvarez@gmail.com>
2019-06-07 21:50:01 -0300

    create order

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const menu = [
-	{ id:0001, name: "Tarta de jamón y queso", type:"Principal", price:100, promo:"1" },
-	{ id:0002, name: "Ensalada caprese" , type:"Principal", price:150, promo:"1" },
-	{ id:0003, name: "Milanesa", type:"Principal", price:200, promo:"1" },
-	{ id:0004, name: "Ensalada mixta", type:"Guarnición", price:120, promo:"1" },
-	{ id:0005, name: "Papas fritas", type:"Guarnición", price:75, promo:"1" },
-	{ id:0006, name: "Puré de zapallo", type:"Guarnición", price:100, promo:"1" },
-	{ id:0007, name: "Flan con crema", type:"Postre", price:90, promo:"1" },
-	{ id:0050, name: "Queso y dulce", type:"Postre", price:90, promo:"1" },
-    { id:0020, name: "Mousse de chocolate", type:"Postre", price:90, promo:"1" },
-    { id:0010, name: "Coca común", type: "Bebida", price:50, promo:"1"},
-    { id:0011, name: "Paso de los toros", type: "Bebida", price:50, promo:"1"},
+	{ id:"0001", name: "Tarta de jamón y queso", type:"Principal", price:100, promo:"1" },
+	{ id:"0002", name: "Ensalada caprese" , type:"Principal", price:150, promo:"1" },
+	{ id:"0003", name: "Milanesa", type:"Principal", price:200, promo:"1" },
+	{ id:"0004", name: "Ensalada mixta", type:"Guarnición", price:120, promo:"1" },
+	{ id:"0005", name: "Papas fritas", type:"Guarnición", price:75, promo:"1" },
+	{ id:"0006", name: "Puré de zapallo", type:"Guarnición", price:100, promo:"1" },
+	{ id:"0007", name: "Flan con crema", type:"Postre", price:90, promo:"1" },
+	{ id:"0050", name: "Queso y dulce", type:"Postre", price:90, promo:"1" },
+    { id:"0020", name: "Mousse de chocolate", type:"Postre", price:90, promo:"1" },
+    { id:"0010", name: "Coca común", type: "Bebida", price:50, promo:"1"},
+    { id:"0011", name: "Paso de los toros", type: "Bebida", price:50, promo:"1"},
 ]
 
 // IDENTIFICAR LOS TIPOS DE PLATOS
 // una lista con todos los tipos de platos del menu
 const allTypes = menu.map(({type}) => type)
+
 // Nuev lista con tipos únicos
 const plateTypes = allTypes.filter((e, i) => allTypes.indexOf(e) === i)
 
@@ -47,6 +48,16 @@ const createOption = elem =>{
     option.innerText = elem.name
     option.value = elem.id
     return option
+}
+
+const createOrder = () => {
+    let order = []
+    plateTypes.forEach( e => {
+        let select = document.getElementById(e)
+        let plate = menu.find( e => e.id === select.value)
+        order.push(plate)
+    })
+    console.log(order)
 }
 
 // inicializacion del programa
